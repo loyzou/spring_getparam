0f4db0c77a2acae71b9aba57cf69274e879dbe7c
sufow <541288359@qq.com>
2016-06-03 16:45:06 +0800

    update

--- day0603_springTest/src/com/sufow/springtest/SpringTest.java
+++ day0603_springTest/src/com/sufow/springtest/SpringTest.java
@@ -1,7 +1,9 @@
 package com.sufow.springtest;
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 import com.sufow.bean.Sufow;
 
@@ -15,6 +17,13 @@ public class SpringTest {
 		Sufow sufow = (Sufow)context.getBean("sufow");
 		
 		System.out.println(sufow.getUsername());
+		
+		//第二种方式
+		XmlBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("applicationContext.xml"));
+		
+		Sufow sufow2 =  (Sufow)beanFactory.getBean("sufow");
+		
+		System.out.println(sufow2.getUsername());
 		
 		
 	}
